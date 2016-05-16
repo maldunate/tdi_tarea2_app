ac231719f8924b4c69049d3e3b826b077e75c511
maldunate <maldunate@uc.cl>
2016-05-16 01:32:21 -0400

    solved3

--- test/controllers/api_controller_test.rb
+++ test/controllers/api_controller_test.rb
@@ -10,13 +10,19 @@ class ApiControllerTest < ActionController::TestCase
 		assert_equal 400, response.status
 	end
 
-	test "tagCount return int or false" do 
+	test "tagCount1" do 
 		assert_equal false, @controller.send(:tagCount, 'chile', '987'), "Bad Token"
+	end
+
+	test "tagCount2" do 
 		assert_not_equal false, @controller.send(:tagCount, 'chile', '2019746130.59a3f2b.86a0135240404ed5b908a14c0a2d9402')
 	end
 
-	test "arrayPosts return content or false" do
+	test "arrayPosts1" do
 		assert_equal false, @controller.send(:arrayPosts, 'chile', '987'), "Bad Token"
+	end
+
+	test "arrayPosts2" do
 		assert_not_equal false, @controller.send(:arrayPosts, 'chile', '2019746130.59a3f2b.86a0135240404ed5b908a14c0a2d9402')
 	end
 
